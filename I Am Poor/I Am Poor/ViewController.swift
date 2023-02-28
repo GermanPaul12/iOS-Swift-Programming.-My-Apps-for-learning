93d62ae238f911eae02f334767edee757f606472
German Paul <99292019+GermanPaul12@users.noreply.github.com>
2023-02-28 08:28:51 +0100

    I am Rich. I am Poor and Playground

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by German Paul on 26.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+// This is a comment
+
+/* This is a multiline comment
+ I can type a lot of lines */
+
+
+// Strings, Variables and Constants
+"I'm a good programmer. The answer to everything is \(6*7)"
+
+var a = 5
+var b = 6
+a + b
+
+let c = 3 // immutable
+a = 4
+a + b * c
+"The sum of a,b and c is \(a+b+c)"
+
+(a,b) = (b,a)
+a
+b
+
+// Arrays
+var numbers = [1,2,3,4,5,6]
+numbers.append(7)
+numbers[6]
+
+var bigNumbers = [45,73,195,53]
+var computedNumbers = [bigNumbers[0]*bigNumbers[1],bigNumbers[1]*bigNumbers[2],bigNumbers[2]*bigNumbers[3]]
+//print(computedNumbers)
+
