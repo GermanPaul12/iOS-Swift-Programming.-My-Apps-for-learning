d7b9ea80860ad1262e0182de341220967d5e0f1d
German Paul <99292019+GermanPaul12@users.noreply.github.com>
2023-02-28 13:36:50 +0100

    Xylophone

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -18,8 +18,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        let buttonTitle = sender.currentTitle!
-        playSound(buttonTitle: buttonTitle)
+        playSound(buttonTitle: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
     
     
